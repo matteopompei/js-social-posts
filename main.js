26bb84f5e9488ad75cf34646eb7951e1fdfa86f7
Matteo Pompei <datxizzan@gmail.com>
2022-01-11 15:43:49 +0100

    Post generati dinamicamente

--- main.js
+++ main.js
@@ -55,3 +55,41 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+
+let postContainer = document.getElementById("container");
+
+for (key in posts) {
+    postContainer.innerHTML += `
+        <div class="post">
+            <div class="post__header">
+                <div class="post-meta">                    
+                    <div class="post-meta__icon">
+                        <img class="profile-pic" src="${posts[key].author.image}" alt="${posts[key].author.name}">                    
+                    </div>
+                    <div class="post-meta__data">
+                        <div class="post-meta__author">${posts[key].author.name}</div>
+                        <div class="post-meta__time">${posts[key].created}</div>
+                    </div>                    
+                </div>
+            </div>
+            <div class="post__text">${posts[key].content}</div>
+            <div class="post__image">
+                <img src="${posts[key].media}" alt="">
+            </div>
+            <div class="post__footer">
+                <div class="likes js-likes">
+                    <div class="likes__cta">
+                        <a class="like-button  js-like-button" href="#" data-postid="${posts[key].id}">
+                            <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                            <span class="like-button__label">Mi Piace</span>
+                        </a>
+                    </div>
+                    <div class="likes__counter">
+                        Piace a <b id="like-counter-${posts[key].id}" class="js-likes-counter">${posts[key].likes}</b> persone
+                    </div>
+                </div> 
+            </div>            
+        </div>
+    `;
+  }
